05e3fa2b68d26cd6059a8f5cc36da27054643b5b
theoriginalsam <spoudel04@gmail.com>
2020-07-26 20:38:33 +0545

    Commit via Script

--- routes/feed.js
+++ routes/feed.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get("/posts", feedController.getPosts);
 router.post("/post", feedController.createPosts);
+router.put("/post/:id", feedController.editPosts);
 
 module.exports = router;
