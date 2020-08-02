b68c3cd4e87fcba387dddfeb393a3a4f88c8be4f
theoriginalsam <spoudel04@gmail.com>
2020-08-02 22:45:08 +0545

    Commit via Script

--- GraphQL/app.js
+++ GraphQL/app.js
@@ -41,10 +41,7 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
