60e056c8318cbbff54a8781cacb14c10b81c4191
theoriginalsam <spoudel04@gmail.com>
2020-08-02 22:44:23 +0545

    Commit via Script

--- GraphQL/app.js
+++ GraphQL/app.js
@@ -67,9 +67,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(
-    "mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/messages?retryWrites=true"
-  )
+  .connect("ypur url")
   .then((result) => {
     app.listen(8080);
   })
