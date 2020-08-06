ee3d9f2f160fee89d8e05ab5382b72b6ff866c3e
theoriginalsam <spoudel04@gmail.com>
2020-08-06 21:13:41 +0545

    Commit via Script

--- GraphQL/controllers/auth.js
+++ GraphQL/controllers/auth.js
@@ -6,12 +6,7 @@ const User = require("../models/user");
 
 exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error("Validation failed.");
-    error.statusCode = 422;
-    error.data = errors.array();
-    throw error;
-  }
+
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
