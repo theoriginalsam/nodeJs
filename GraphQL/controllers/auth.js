1ea0331ad81d91867c1f3e19bc70c6f87537ea5c
theoriginalsam <spoudel04@gmail.com>
2020-08-06 21:14:07 +0545

    Commit via Script

--- GraphQL/controllers/auth.js
+++ GraphQL/controllers/auth.js
@@ -1,6 +1,12 @@
 const { validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+exports.signup = async (req, res, next) => {
+  const errors = validationResult(req);
+
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
@@ -14,13 +20,7 @@ const jwt = require("jsonwebtoken");
     });
     const result = await user.save();
     res.status(201).json({ message: "User created!", userId: result._id });
-  } catch (err) {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  }
-};
+ 
 
 exports.login = async (req, res, next) => {
   const email = req.body.email;
@@ -35,7 +35,11 @@ exports.login = async (req, res, next) => {
     }
     loadedUser = user;
     const isEqual = await bcrypt.compare(password, user.password);
-   
+    if (!isEqual) {
+      const error = new Error("Wrong password!");
+      error.statusCode = 401;
+      throw error;
+    }
     const token = jwt.sign(
       {
         email: loadedUser.email,
