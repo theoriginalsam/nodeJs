9c1162520529d6cfe0af0ef79dc2ab167aa793c5
theoriginalsam <spoudel04@gmail.com>
2020-08-06 21:13:34 +0545

    Commit via Script

--- GraphQL/controllers/auth.js
+++ GraphQL/controllers/auth.js
@@ -70,12 +70,6 @@ exports.login = async (req, res, next) => {
 
 exports.getUserStatus = async (req, res, next) => {
   try {
-    const user = await User.findById(req.userId);
-    if (!user) {
-      const error = new Error("User not found.");
-      error.statusCode = 404;
-      throw error;
-    }
     res.status(200).json({ status: user.status });
   } catch (err) {
     if (!err.statusCode) {
